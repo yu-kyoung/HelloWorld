b8865f727e5d1f18f9ca5b7a16d1b5e829eca538
user <px@px.com>
2025-05-14 12:38:15 +0900

    conflict 연습

--- HelloWorld/src/com/yedam/JSExe.java
+++ HelloWorld/src/com/yedam/JSExe.java
@@ -2,14 +2,7 @@ package com.yedam;
 
 public class JSExe {
 	public static void main(String[] args) {
-		// 임의의 수를 생성.1~100사이의 임의의 수 생성
-		int sum = 0;
-		for (int i = 1; i <= 5; i++) {
-			int result = (int) (Math.random() * 100) + 1;// 0<= x <1 실수 캐스팅 중요
-			sum = +result;
-		}
-		System.out.println("결과: " + sum);
-
+		test2();
 	}// end of main
 
 	public static void test() {
@@ -23,4 +16,16 @@ public class JSExe {
 		System.out.println("결과: " + sum);
 
 	}// end of test
+
+	public static void test2() {
+		// 임의의 수를 생성.1~100사이의 임의의 수 생성
+		int sum = 0;
+		for (int i = 1; i <= 5; i++) {
+			int result = (int) (Math.random() * 100) + 1;// 0<= x <1 실수 캐스팅 중요
+			sum = +result;
+		}
+		System.out.println("결과: " + sum);
+
+	}//end of test2
+
 }// end class
