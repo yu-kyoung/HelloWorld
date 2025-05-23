fc9594f3d2450be652c6dea4db54621cdae3dff5
user <px@px.com>
2025-05-23 09:32:34 +0900

    ?3

--- HellowClass/src/com/yedam/api/StringExe.java
+++ HellowClass/src/com/yedam/api/StringExe.java
@@ -1,5 +1,7 @@
 package com.yedam.api;
 
+import com.yedam.member.Member;
+
 public class StringExe {
 	public static void main(String[] args) {
 		
@@ -10,5 +12,13 @@ public class StringExe {
 		String str1=new String("Hello");
 		String str2=new String("Hello");
 		System.out.println(str1.equals(str2));
+		
+		Member mem1 =new Member();//인스턴스
+		Member mem2 =new Member();//주소비교
+
+		System.out.println(mem1==mem2);
+		System.out.println(mem1.equals(mem2));
+
+		
 	}
 }
